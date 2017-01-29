f0803e65f06d04ab6fc272f8c099ec254e3323b1
Meruva, Sivakumari (ASPL) <Sivakumari.Meruva@allstate.com>
2017-01-29 12:27:20 +0530

    Test branch 2

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -27,7 +27,7 @@
     test.attributedText = str;
     test.editable = false;
     test.scrollEnabled = false;
-    NSLog(@"Test Branch 1");
+    NSLog(@"Test Branch 2")
   }
 
 - (BOOL) checkEvenNumber:(int)number {
