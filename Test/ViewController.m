d0c5504267fec494aa62315f1d433ab9cc10ae2e
Sivakumari <Sivakumari.Meruva@allstate.com>
2017-07-12 14:37:35 +0530

    Test branch- 12

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -28,7 +28,7 @@
     test.editable = false;
     test.scrollEnabled = false;
     
-    NSLog(@"Testing repo merge conflicts");
+    NSLog(@"test branch 12");
   }
 
 - (BOOL) checkEvenNumber:(int)number {
