2b1a77b9e94b6998e1e46d4f0b5917fd6725d2fc
Sivakumari <Sivakumari.Meruva@allstate.com>
2017-08-13 23:39:56 +0530

    Test master branch 2

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -28,7 +28,7 @@
     test.editable = false;
     test.scrollEnabled = false;
     
-    NSLog(@"Test blue ocean fail buid")
+    NSLog(@"Test branch master")
   }
 
 - (BOOL) checkEvenNumber:(int)number {
