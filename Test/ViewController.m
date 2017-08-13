92c4b043e9133bd6884f3adf0e5a0d9205e6c427
Sivakumari <Sivakumari.Meruva@allstate.com>
2017-08-13 23:15:35 +0530

    Test blue ocean

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -28,7 +28,7 @@
     test.editable = false;
     test.scrollEnabled = false;
     
-    NSLog(@"Testing repo merge conflicts");
+    NSLog(@"Test blue ocean");
   }
 
 - (BOOL) checkEvenNumber:(int)number {
