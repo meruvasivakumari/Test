66d39b57dc6a005aeb12c5217fb83510a3783fc2
Sivakumari <Sivakumari.Meruva@allstate.com>
2017-08-20 23:31:15 +0530

    Test branch compilation should success

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -28,7 +28,7 @@
     test.editable = false;
     test.scrollEnabled = false;
     
-    NSLog(@"Test blue ocean fail buid")
+    NSLog(@"Test branch pipeline");
   }
 
 - (BOOL) checkEvenNumber:(int)number {
